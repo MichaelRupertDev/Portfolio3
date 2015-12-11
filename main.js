324b0747b73fb7c36aef4adcd533cac662f8c817
Michael Rupert <mbrupert@iastate.edu>
2015-12-11 16:56:13 -0600

    Got basic drawing functionality working

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/canvas.html');
+  res.sendFile(__dirname + '/html/canvas.html');
+});
+
+io.on('connection', function(socket){
+	console.log('User connected.');
+	socket.on('shape', function(Shape){
+		console.log('Shape recieved: ' + Shape);
+		io.emit('shape', Shape);
+	});
 });
 
 http.listen(3000, function(){
